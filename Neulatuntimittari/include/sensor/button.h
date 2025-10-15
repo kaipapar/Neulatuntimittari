4bdc7d2856b785b5d4d84edd9670f63c3e4fa284
Karri Korsu <kkorsu@gmail.com>
2025-10-15 16:57:49 +0300

    Add button functionality. Not implemented

--- Neulatuntimittari/include/sensor/button.h
+++ Neulatuntimittari/include/sensor/button.h
@@ -6,10 +6,16 @@ Karri Korsu 2025
 #pragma once
 #include <stdint.h>
 #include <Arduino.h>
+#include "storage/csv.h"
 #define BTN_PIN
 #define SLOW_THRHLD 5000
+#define SUPER_SLOW 8000 // if button is accidentally pressed for too long
+#define DEBOUNCE 100
 
 void IRAM_ATTR btn_isr();
 void setup_btn();
-int8_t fast_click(); // Cycle through styli
-int8_t slow_click(); // Set current stylus hours to 0
+int8_t fast_click(uint64_t active_styli[COLS]); // Cycle through styli
+int8_t slow_click(uint64_t* hours); // Set current stylus hours to 0
+int8_t btn_release(uint64_t array[ROWS][COLS]);
+int8_t click_logic(uint64_t array[ROWS][COLS]);
+int8_t get_active(uint64_t array[COLS]);
--- Neulatuntimittari/src/sensor/button.cpp
+++ Neulatuntimittari/src/sensor/button.cpp
@@ -3,3 +3,62 @@ button functionality
 not a sensor but hey
 Karri Korsu 2025
 */
+
+#include "sensor/button.h"
+#include "time/time.h"
+
+volatile int64_t btn_timestamp = 0;
+
+/* Interrupt function for polling push button */
+void IRAM_ATTR btn_isr() 
+{
+  btn_timestamp = current_time_ms();
+}
+
+void setup_btn()
+{
+  pinMode(BTN_PIN, INPUT_PULLUP);
+  attachInterrupt(BTN_PIN, btn_isr, CHANGE); // Should it be rising or falling
+}
+/* Cycle through styli */
+int8_t fast_click(uint64_t active_styli[COLS])
+{
+  int8_t current_i = get_active(active_styli);
+  active_styli[current_i] = 0;
+  if (current_i < COLS)
+    active_styli[current_i+1] = 1;    
+  else 
+    active_styli[0] = 1;
+  return 0;
+}
+
+/* Set current stylus hours to 0 */
+int8_t slow_click(uint64_t* hours)
+{
+  *hours = 0;
+  return 0;
+}
+
+int8_t btn_release(uint64_t table[ROWS][COLS])
+{
+  int8_t pressed = !digitalRead(BTN_PIN);
+  if (pressed) {
+    return 1;
+  } else {
+    click_logic(table);
+    btn_timestamp = 0;
+  }
+  return 0;
+}
+
+int8_t click_logic(uint64_t table[ROWS][COLS])
+{
+  int64_t diff = btn_timestamp ? (current_time_ms() - btn_timestamp) : 0;
+  if (diff >= DEBOUNCE && diff < SUPER_SLOW){
+    if (diff >= SLOW_THRHLD)
+      return slow_click(&table[0][get_active(table[1])]);
+    else
+      return fast_click(table[1]);
+  }// do nothing
+  return 1;
+}
